05584b343a09a15e28472b0d930904faee27e008
fantasyui-com <captain@fantasyui.com>
2018-10-11 06:41:37 -0400

    ECMAScript Modules

---
+++ index.mjs
@@ -0,0 +1,24 @@
+export default function(notes){
+
+  const response = [];
+
+  notes.trim()
+  .split(/\n/) // split into lines
+  .map(i=>i .trim() .split('') ) // split line into cells
+    .map(line=>
+
+      line
+      .map(i=>i.match(/[^x-]/)?null:i) // mark non 'x'/'-' as null
+      .filter(i=>i) // throw away nulls
+      .map(i=>i==='-'?0:i) // convert '-' to 0
+      .map(i=>i==='x'?1:i) // convert 'x' to 1
+
+    )
+  .forEach(function(line){
+    response.push(line)
+  })
+
+
+  return response;
+
+};
